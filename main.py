a0d920bc5055de02fbb3bd5b1baa7399dc930147
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 21:39:05 +0400

    Add a Label then Pack it

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import tkinter
 
+# Default Settings
 window = tkinter.Tk()
 window.title("Taha First GUI Program")
 window.minsize(width=500, height=300)
+
+# Add a Label then Pack it
+my_label = tkinter.Label(text="I am a Label")
+my_label.pack()
+
+
+
+
+
+
+
 
 
 # Keep the Window Open
