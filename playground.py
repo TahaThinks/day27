dd890d552e7accb4a65c396a255e0d3b81e41b1c
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 22:29:46 +0400

    Class using Keyword Argument

--- playground.py
+++ playground.py
@@ -6,14 +6,28 @@
 #     print(sum)
 # add(1,2,3,4,5,6,7,8,9,10)
 
+# Keyword Argument
+# def calculate(value, **kwargs):
+#     # print(kwargs)
+#     # for key, value in kwargs.items():
+#     #     print(key, value)
+#     value += kwargs["add"]
+#     value *= kwargs["multiply"]
+#     print(value)
+#
+#
+# calculate(5, add=3, multiply=5)
 
-def calculate(value, **kwargs):
-    # print(kwargs)
-    # for key, value in kwargs.items():
-    #     print(key, value)
-    value += kwargs["add"]
-    value *= kwargs["multiply"]
-    print(value)
+
+class Car:
+    def __init__(self, **kwargs):
+        self.make = kwargs["make"]
+        self.model = kwargs["model"]
+        self.year = kwargs["year"]
+
+    def car_info(self):
+        print(f"Car is {self.make} {self.model}")
 
 
-calculate(5, add=3, multiply=5)
+my_car = Car(make="BMW", model="X3", year=2022)
+my_car.car_info()
