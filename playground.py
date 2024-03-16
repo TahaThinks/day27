62228e6dc0bf12786701784f7211af35ccc419d0
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 22:19:09 +0400

    Keyword Arguments Test

--- playground.py
+++ playground.py
@@ -1,8 +1,19 @@
-def add(*args):
-    sum=0
-    for n in args:
-        sum += n
-    print(sum)
+# Positional Argument
+# def add(*args):
+#     sum=0
+#     for n in args:
+#         sum += n
+#     print(sum)
+# add(1,2,3,4,5,6,7,8,9,10)
 
-add(1,2,3,4,5,6,7,8,9,10)
 
+def calculate(value, **kwargs):
+    # print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key, value)
+    value += kwargs["add"]
+    value *= kwargs["multiply"]
+    print(value)
+
+
+calculate(5, add=3, multiply=5)
