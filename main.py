3d42a7791fd732713b9f78256f2773f424dc56a6
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 22:51:28 +0400

    Create a Button

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import tkinter
+from tkinter import *
+
+def button_clicked():
+    print("I got clicked")
 
 # Default Settings
-window = tkinter.Tk()
+window = Tk()
 window.title("Taha First GUI Program")
 window.minsize(width=500, height=300)
 
 # Add a Label then Pack it
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
 my_label["text"] = "New Text Access by Key"
 my_label.config(text="New Text Access by Config")
 
-
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 
