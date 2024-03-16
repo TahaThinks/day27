38e2673102359b465e95f017d785d03ee118c093
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 22:46:02 +0400

    Access Label through config

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-
-
-
+my_label["text"] = "New Text Access by Key"
+my_label.config(text="New Text Access by Config")
 
 
 
