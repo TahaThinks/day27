9dd713e30d70941c157dc28b950e0a4c2af2c250
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-16 21:37:14 +0400

    Add Title & Resize

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("Taha First GUI Program")
+window.minsize(width=500, height=300)
 
 
-
-#Keep the Window Open
+# Keep the Window Open
 window.mainloop()
